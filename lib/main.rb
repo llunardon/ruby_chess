38230b2d716c4cac453b1a3ff9d1399f3005f617
llunardon <lorenzo.lunardon.1@gmail.com>
2022-11-15 16:44:07 +0100

    Modified player.rb initialize method
    Player's initialization method now includes the choice of the color, in case the input parameter is 'new'. Cleaned up the main.rb file a bit.

--- lib/main.rb
+++ lib/main.rb
@@ -8,35 +8,33 @@ class Game
   attr_accessor :board, :player1, :player2
 
   def play_game(choice)
-    #chose to create a new game
+    # chose to create a new game
     if choice == 'new' 
-      #create player 1 and let them choose the color
-      @player1 = Player.new
-      puts "#{@player1.name}, what color do you choose?"
-      @player1.color = gets.chomp.downcase
-      until @player1.color == 'black' || @player1.color == 'white' do
-        @player1.color = gets.chomp.downcase
-      end
+      # create player 1 and let them choose the color
+      @player1 = Player.new(choice)
 
-      #create player 2 and assign them the opposite color
-      @player2 = Player.new
+      # create player 2 and assign them the opposite color
+      @player2 = Player.new('')
       @player1.color == 'white' ? @player2.color = 'black' : @player2.color = 'white'
 
-      #inizialize board 
+      # inizialize board and print it
       @board = Board.new
       @board.print_board
+
       assign_possible_moves(@board)
-    #choice to load a previous game
+
+    # choice to load a previous game
     elsif choice == 'load' 
       yaml = Game.load_game
       @board = yaml.board
       @player1 = yaml.player1
       @player2 = yaml.player2
       @board.print_board
+
       assign_possible_moves(@board)
     end
 
-    #play rounds
+    # play rounds
     loop do
       if @player1.color == 'white'
         play_round(@board, @player1)
@@ -49,24 +47,24 @@ class Game
   end
 
   def play_round(board, player)
-    #remove illegal moves
+    # remove illegal moves
     delete_moves_that_cause_check(board, player)
 
-    #control if the player is in checkmate
+    # control if the player is in checkmate
     if get_all_player_moves(board, player.color).empty?
       puts "#{player.name} lost!"
-      #player is in checkmate -> exit 
+      # player is in checkmate -> exit 
       exit
     end
 
-    #control if the player is in check
+    # control if the player is in check
     puts "***** #{player.name}, you\'re in check! *****" if is_in_check?(board, player)
 
-    #inizialize coordinates
+    # inizialize coordinates
     start_coords = nil
     end_coords = nil
 
-    #get the coordinates of the piece to move
+    # get the coordinates of the piece to move
     puts "It is #{player.name}\'s turn. Choose a piece to move or type \"quit\". The game will be saved automatically every two turns."
     puts "Insert the row and column separated by a comma"
     loop do
@@ -74,13 +72,13 @@ class Game
 
       piece = board.cell_at(start_coords[0], start_coords[1]).piece
 
-      #loop until the starting cell is valid
+      # loop until the starting cell is valid
       break if piece.color == player.color && piece.possible_moves.any?
 
       puts 'Insert a legal move'
     end
 
-    #get the coordinates of the target cell
+    # get the coordinates of the target cell
     puts "Where do you want to move it?"
     loop do
       piece = board.cell_at(start_coords[0], start_coords[1]).piece
@@ -88,31 +86,31 @@ class Game
 
       end_coords = get_input_coords(board)
 
-      #loop until the end cell is valid
+      # loop until the end cell is valid
       break if piece.possible_moves.include?(end_coords) 
 
       puts 'Insert a legal move'
     end
 
-    #move the selected piece to the target cell
+    # move the selected piece to the target cell
     board.move_piece(start_coords, end_coords)
 
-    #update the possible moves
+    # update the possible moves
     assign_possible_moves(board)
 
     board.print_board
     Game.save_game(self) if player.color == 'black'
   end
 
-  #receive the coordinates in stdin
+  # receive the coordinates in stdin
   def get_input_coords(board)
     loop do
       input = gets.chomp
-      #the player wants to quit the game
+      # the player wants to quit the game
       exit if input.downcase == 'quit'
 
       input = input.split(',') 
-      #clean up the input
+      # clean up the input
       input[0] = input[0].to_i 
       input[1] = input[1].to_s.strip 
 
@@ -132,17 +130,16 @@ class Game
 end
 
 puts "Do you want to start a new game or load the saved one? Type \"new\" or \"load\""
-input = ''
+choice = ''
 loop do
-  input = gets.chomp
-  break if input == 'new' || input == 'load'
+  choice = gets.chomp
+  break if choice == 'new' || choice == 'load'
 end
 
-if input == 'load' && !(File.file?('save_file.yml'))
+if choice == 'load' && !(File.file?('save_file.yml'))
   puts 'the save file does not exist, creating a new game.'
-  input = 'new'
+  choice = 'new'
 end
 
 game = Game.new
-game.play_game(input)
-
+game.play_game(choice)
--- lib/player.rb
+++ lib/player.rb
@@ -1,9 +1,20 @@
 class Player
   attr_accessor :name, :color
 
-  def initialize()
+  def initialize(choice)
     puts "What\'s your name?"
     @name = gets.chomp
-    @color = ''
+
+    if choice == 'new'
+      puts "#{@name}, what color do you choose?"
+      @color = gets.chomp.downcase
+
+      until @color == 'black' || @color == 'white' do
+        @color = gets.chomp.downcase
+      end
+
+    else
+      @color = ''
+    end
   end
 end
